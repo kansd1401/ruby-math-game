5acab25187ede66d91a962f7c9b4e8be05adbf6c
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-12-09 17:08:47 -0800

    small change

--- mian.rb
+++ mian.rb
@@ -3,11 +3,11 @@ require "./game"
 game = Game.new("Mario","Green Mario")
 
 while game.h1 > 0 && game.h2 > 0
+  puts "----- NEW TURN -----"
   puts game.question
   game.answer = gets.chomp.to_i
   puts game.checkResult
   puts game.score
-  puts "----- NEW TURN -----"
 end
 
 puts game.winner
